2e797c7ffc61675a368ce6aabc14118f488a4a59
Talha_Durmaz <144232044+talhadrz@users.noreply.github.com>
2024-09-14 14:26:00 +0300

    Update Şifre çöz.cs
    Signed-off-by: Talha_Durmaz <144232044+talhadrz@users.noreply.github.com>

--- metin şifreleme/Şifre çöz.cs
+++ metin şifreleme/Şifre çöz.cs
@@ -1,14 +1,11 @@
 public static string sıfre_coz(string mtn)
 {
-    int ılk = ToInt32(mtn[0]) / 2;
-    string mtn2 = null;
-    int sayı;
-    for (int i = 1; i < mtn.Length; i++)
+    string donecek = null;
+    for (int ılk = mtn[0], s1, i = 1; i < mtn.Length; i++)
     {
-        sayı = ToInt32(mtn[i]);
-        sayı -= ToInt32(ılk * 2);
-        sayı = sayı / 2;
-        mtn2 = mtn2 + ToChar(sayı);
+        s1 = mtn[i];
+        s1 = (s1 - ılk) / 2;
+        donecek = donecek + ToChar(s1);
     }
-    return mtn2;
+    return donecek;
 }
