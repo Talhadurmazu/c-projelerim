76e817ca77273d0a3f3b6f04e921ee5dd136ea81
Talha_Durmaz <144232044+talhadrz@users.noreply.github.com>
2024-09-14 13:37:15 +0300

    Update Main.cs
    Signed-off-by: Talha_Durmaz <144232044+talhadrz@users.noreply.github.com>

--- metin şifreleme/Main.cs
+++ metin şifreleme/Main.cs
@@ -1,11 +1,12 @@
 static void Main(string[] args)
 {
-Write("sayı: ");
-string mtn = ReadLine();
-mtn = sıfrele(mtn);
-
-WriteLine();
-WriteLine(mtn);
-WriteLine();
-WriteLine(sıfre_coz(mtn));
+    Write("Metin: ");
+    string mtn = ReadLine();
+    string sıfre = sıfrele(mtn);
+    WriteLine();
+    WriteLine($"Orjinal Metin: {mtn}");
+    WriteLine();
+    WriteLine($"Şifreli Metin: {sıfre}");
+    WriteLine();
+    WriteLine($"Çözülen Şifre: {sıfre_coz(sıfre)}");
 }
